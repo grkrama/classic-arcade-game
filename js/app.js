ca079925f86e800695ee7cfb3993bcda2a50d669
Karthik Ramachandran <KRamachandran@zebra.com>
2019-03-31 21:57:39 -0500

    Added Code To Reset the player when reaches water.

--- js/app.js
+++ js/app.js
@@ -13,6 +13,7 @@ var Enemy = function(x, y, speed) {
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
+
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
@@ -27,6 +28,12 @@ Enemy.prototype.update = function(dt) {
         player.x + 37 > this.x &&
         player.y < this.y + 25 &&
         30 + player.y > this.y) {
+        player.playerReset();
+    }
+
+    if (player.y == 0)
+    {
+        alert("Game Over");
         player.playerReset();
     }
 
